639e865a972643b9105156b6ab7e0f819560e913
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2022-12-20 17:48:10 +0900

    add auto build to jenkins

---
+++ Assets/Editor/BuildPlayer.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using System;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildPlayer : MonoBehaviour
+{
+    public static void JenkinsBuilder()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        string[] scenes = UnityEditor.EditorBuildSettingsScene.GetActiveSceneList(UnityEditor.EditorBuildSettings.scenes);
+        buildPlayerOptions.scenes = scenes;
+        buildPlayerOptions.locationPathName = "BuildT/RunRunRun.exe";
+        //buildPlayerOptions.locationPathName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
