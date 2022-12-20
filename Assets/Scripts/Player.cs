63fac5e7d05550e2583d81ff61f77f0dd46550de
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2022-12-20 21:27:46 +0900

    add jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,14 +9,27 @@ public class Player : MonoBehaviour
     [SerializeField] Transform MiddlePos;
     [SerializeField] Transform RightPos;
 
-    [SerializeField] float speed = 0f;
-
     Transform targetPos = null;
     [SerializeField] int myPos = 0;
+    [SerializeField] float speed = 0f;
+
+    [SerializeField] bool isGround = true;
+
+    [Header("기획자 분들을 위한 Zone")]
+    [SerializeField] float jumpPower = 0f;
+
+
+    WaitForFixedUpdate time = new WaitForFixedUpdate();
+    float deltaTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
         targetPos = MiddlePos;
+    }
+
+    private void Awake()
+    {
+        deltaTime = Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -30,6 +43,12 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             myPos = ++myPos > 1 ? 1 : myPos;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGround)
+        {
+            isGround = false;
+            StartCoroutine(Jump(0.5f));
         }
 
         switch (myPos)
@@ -49,5 +68,31 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         transform.position = Vector3.Lerp(gameObject.transform.position, targetPos.transform.position, 0.2f * speed);
+    }
+
+    IEnumerator Jump(float time)
+    {
+        while (true)
+        {
+            time -= deltaTime;
+            if (time < 0)
+            {
+                yield break;
+            }
+            transform.position += Vector3.up * time * jumpPower;
+            yield return time;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
     }
 }
